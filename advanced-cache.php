146bcd6f4030bb47b61360482c37f61e8fd95186
Jeroen Kloppenburg <95744565+webatleten@users.noreply.github.com>
2025-01-27 16:46:18 +0100

    Update advanced-cache.php
    Updated code to 1.1

--- advanced-cache.php
+++ advanced-cache.php
@@ -4,10 +4,10 @@
  *
  * @package      	  WA
  * @author        	  Webatleten
- * Description:       Simple WordPress page caching
+ * Description:       One file to cache all of WordPress
  * Version:           1.1
  * Author:            Webatleten
- * Author URI:        https://Webatleten.nl/
+ * Author URI:        https://webatleten.nl/
 */
 
 // check if cache is enabled
@@ -17,11 +17,12 @@ if( !defined( 'WP_CACHE' ) || !WP_CACHE ) {
 
 $wa_cache = true;
 
-// these are empty on bash
+// these are empty on bash (CLI)
 if( empty( $_SERVER[ 'SERVER_NAME' ] ) || empty( $_SERVER[ 'REQUEST_URI' ] ) ) {
     $wa_cache = false;
 }
 
+// create and check for a session
 $session = array();
 
 if( !empty( $_SESSION ) ) {
@@ -33,7 +34,7 @@ if( isset( $session[ 'easyflex' ] ) && !$session[ 'easyflex' ] ) {
     unset( $session[ 'easyflex' ] );
 }
 
-// check if the user is doing something
+// check on basic WP and user actions
 if( is_admin() || wp_doing_ajax() || !empty( $session ) || !empty( $_POST ) || !empty( $_FILES ) ) {
     $wa_cache = false;
 }
@@ -60,7 +61,7 @@ if ( $wa_cache && !empty( $_GET ) ) {
     }
 }
 
-// check request for a file
+// check request for a file request
 if( $wa_cache && $_SERVER[ 'REQUEST_URI' ] != '/' && preg_match( '|\.|', $_SERVER[ 'REQUEST_URI' ] ) ) {
     $wa_cache = false;
 }
@@ -84,7 +85,11 @@ if( !file_exists( $wa_cache_dir ) ) {
 $wa_cache_dir_htaccess = $wa_cache_dir.'.htaccess';
 
 if( !file_exists( $wa_cache_dir_htaccess ) ) {
-    file_put_contents( $wa_cache_dir_htaccess, '<Files "*"> Require all denied </Files>' );
+    file_put_contents( $wa_cache_dir_htaccess, trim( "
+<Files \"*\">
+    Order Allow,Deny
+    Deny from all
+</Files>" ) );
 }
 
 // create cache file based on URL
@@ -92,34 +97,22 @@ global $wa_cache_file;
 
 $wa_cache_file = $wa_cache_dir.md5( $_SERVER[ 'SERVER_NAME' ].$_SERVER[ 'REQUEST_URI' ] ).'.html';
 
-// get cache settings
-global $wa_cache_settings;
+$wa_cache_timeout = 43200;  //default 12 hours
 
-$wa_cache_settings = (object) array(
-    'timeout'  => 43200, //12 hours
-    'minify'   => 1 //minify html
-);
-
-if( file_exists( ABSPATH.'wp-content/settings/wa/optimize.json' ) ) {
-    $wa_cache_saved_settings = json_decode( file_get_contents( ABSPATH.'wp-content/settings/wa/optimize.json' ) );
-
-    if( !empty( $wa_cache_saved_settings->timeout ) && $wa_cache_saved_settings->timeout === 'true' ) {
-        $wa_cache_settings->timeout = 0;
-    }
-
-    if( !empty( $wa_cache_saved_settings->minify ) && $wa_cache_saved_settings->minify === 'false' ) {
-        $wa_cache_settings->minify = 0;
-    }
+// get custom timemout
+if( file_exists( ABSPATH.'wp-content/settings/wa/timeout.txt' ) ) {
+    $wa_cache_timeout = (float) file_get_contents( ABSPATH.'wp-content/settings/wa/timeout.txt' );
 }
 
 // remove existing cache file after timeout
-if( $wa_cache_settings->timeout > 0 && file_exists( $wa_cache_file ) && ( time() - filemtime( $wa_cache_file ) ) > $wa_cache_settings->timeout ) {
+if( $wa_cache_timeout > 0 && file_exists( $wa_cache_file ) && ( time() - filemtime( $wa_cache_file ) ) > $wa_cache_timeout ) {
     unlink( $wa_cache_file );
 }
 
 // load cache file
 if( file_exists( $wa_cache_file ) ) {
 	if( $contents = file_get_contents( $wa_cache_file ) ) {
+        // check if cache file has all basic HTML elements
 		if( preg_match( '/html|head|body/', $contents ) ) {
 			echo $contents; exit;
 		}
@@ -132,6 +125,7 @@ ob_start();
 // save cache file (after some checks)
 function wa_cache_save()
 {
+    // recheck session
 	$session = array();
 
     if( !empty( $_SESSION ) ) {
@@ -143,6 +137,7 @@ function wa_cache_save()
         unset( $session[ 'easyflex' ] );
     }
 
+    //if session is not empty or its a 404 page, don't cache
     if( is_404() || !empty( $session ) ) {
         return;
     }
@@ -152,40 +147,26 @@ function wa_cache_save()
     if( !isset( $wa_cache_done ) ) {
         $wa_cache_done = false;
     }
+    // stop if the page is already cached
     else if( $wa_cache_done ) {
         return;
     }
 
     if( $cache_data = ob_get_clean() ) {
-	    global $wa_cache_file, $wa_cache_settings;
+	    global $wa_cache_file;
 
-        if( $wa_cache_settings->minify ) {
-            // Verwijder HTML opmerkingen (behalve IE voorwaarden)
-            $cache_data = preg_replace('/<!--(?!\[if.*?\]).*?-->/', '', $cache_data);
+        // do stuff with the cache_data
+        $cache_data = apply_filters( 'wa_cache_data', $cache_data );
 
-            // Verwijder overtollige witruimtes tussen HTML tags
-            $cache_data = preg_replace('/>\s+</', '><', $cache_data);
-
-            // Verwijder tabs, nieuwe regels, en overtollige spaties
-            $cache_data = preg_replace('/\s{2,}/', ' ', $cache_data); 
-            
-            // Meerdere spaties vervangen door één spatie
-            //$cache_data = str_replace(["\n", "\r", "\t"], '', $cache_data); 
-            
-            // Nieuwe regels, tabs verwijderen
-            $cache_data = trim ($cache_data );
-        }
-
+        //check if result if cache file is available and there are basic HTML tags
         if( !empty( $wa_cache_file ) && preg_match( '/html|head|body/', $cache_data ) ) {
-            $cache_data = str_replace( '"width=device-width, user-scalable=no, minimum-scale=1, maximum-scale=1, initial-scale=1.0"', '"width=device-width, initial-scale=1.0"', $cache_data );
-
             file_put_contents( $wa_cache_file, $cache_data );
             chmod( $wa_cache_file, 0777 );
 
             $wa_cache_done = true;
-	}
+	    }
 
-	echo $cache_data;
+	    echo $cache_data;
     }
 }
 
